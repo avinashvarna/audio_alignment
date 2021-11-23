0dd064d1f83f0c600daae45b0e188b90708d86d0
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2021-11-24 03:02:22 +0530

    re-align ashtadhyayi with a mechanism to ignore text in [brackets]

---
+++ ashtadhyayi/align.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Align Ashtadhyayi text with audio
+
+Text source: https://ashtadhyayi.com
+Audio: https://www.sanskritfromhome.in/course/ashtadhyayi/
+
+@author: Hrishikesh Terdalkar
+"""
+
+import os
+import sys
+import csv
+import time
+
+from tqdm import tqdm
+
+base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(base_dir)
+
+try:
+    from utils.alignment import align, clean
+except ImportError:
+    pass
+
+
+def get_text_audio_map():
+    map_file = 'text_audio_map.csv'
+
+    text_audio_map = []
+    with open(map_file, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            text_audio_map.append(row)
+
+    return text_audio_map
+
+
+def align_text(text_audio_map, align_out_dir, word_align=False):
+    os.makedirs(align_out_dir, exist_ok=True)
+
+    for text_path, audio_path in tqdm(text_audio_map):
+        clean_text_path = f"{text_path}.clean"
+        clean(text_path, clean_text_path)
+        audio_basename = os.path.basename(audio_path)
+        audio_basename = os.path.splitext(audio_basename)[0]
+        align_path = os.path.join(align_out_dir, f'{audio_basename}.json')
+        if not os.path.exists(align_path):
+            align(clean_text_path, audio_path, align_path, word_align)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    text_audio_map = get_text_audio_map()
+
+    # align_out_dir = 'sentence_alignment'
+    # align_text(text_audio_map, align_out_dir, word_align=False)
+
+    align_out_dir = 'word_alignment'
+    align_text(text_audio_map, align_out_dir, word_align=True)
+
+    print(f'Finished in {time.time()-start_time:0.4f} seconds.')
